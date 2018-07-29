b5456dc4e4886e97b85faed6cdc59699fbfff4d3
rowlindsay <rowlindsay@gmail.com>
2018-07-28 20:55:39 -0700

    pca colormap

--- visualize.py
+++ visualize.py
@@ -16,16 +16,20 @@ feature_set = pd.read_csv('data/data.csv')
 
 #PCA
 X = np.transpose(feature_set.values)
-m = X.size
+m = X.shape[0]
 covar_mat = np.dot(np.transpose(X),X) / m
 U, S, V = linalg.svd(covar_mat)
 U_reduce = U[:,0:3]
 Z_all = np.dot(X,U_reduce)
 
-
+color_map = {}
+color_map['colors']=['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
+color_map['names']=['r/gadgets','r/sports','r/gaming','r/pics','r/worldnews','r/videos','r/AskReddit','r/aww','r/funny','r/news']
 ax = plt.axes(projection='3d')
 x = Z_all[:,0]
 y = Z_all[:,1]
 z = Z_all[:,2]
-ax.scatter(x, y, z, c=z, cmap='viridis', linewidth=0.5);
+for i in range(0,m-1):
+        ax.scatter(x[i], y[i], z[i], c=color_map['colors'][i], cmap='plasma', label=color_map['names'][i])
+plt.legend()
 plt.show()
